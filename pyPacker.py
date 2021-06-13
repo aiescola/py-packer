8e155c8f1f3ffc2d513d811ac03d610f0bb5b028
Aitor <aitor.escolar@bnext.es>
2021-06-14 00:56:59 +0200

    add argparser. Methods to remove original and compressed folders and make compression optional

--- pyPacker.py
+++ pyPacker.py
@@ -1,12 +1,15 @@
-from os import walk, rename, makedirs
-from os.path import curdir, splitext
-from posixpath import abspath, dirname, basename
-from subprocess import DEVNULL, PIPE, Popen
-import sys
+#!/usr/bin/python3
 
-def optimize(inputFolder):
-    print("Optimizing folder: " + inputFolder)
-    outputFolder = dirname(inputFolder) + "/" + basename(inputFolder) + "_optimized"
+import argparse
+import os
+from os import walk, rename, makedirs
+from os.path import  splitext
+from posixpath import  dirname, basename
+from subprocess import DEVNULL, PIPE, Popen
+
+def compress(inputFolder):
+    print("Compressing folder: " + inputFolder)
+    outputFolder = dirname(inputFolder) + "/" + basename(inputFolder) + "_compressed"
 
     makedirs(outputFolder, exist_ok=True)
 
@@ -17,9 +20,11 @@ def optimize(inputFolder):
         '-format', 'jpg',
         '-colorspace', 'sRGB',
         '-sampling-factor', '4:2:0',
-        '-quality', '75', 
+        '-quality', '80', 
+        #'-resize', '100%',
         '-path', outputFolder,
-        inputFolder + "/*\.jp*g"]).communicate()
+        inputFolder + "/*\.jp*g"], 
+        stdout=DEVNULL, stderr=DEVNULL).communicate()
     
     return(outputFolder)
 
@@ -28,20 +33,67 @@ def sevenzip(inputFolder):
     print("creating archive at " + outputFolder + " from " + inputFolder)
     system = Popen(
         ['7z', 'a', '-tzip', inputFolder, inputFolder]
-        , stdout=DEVNULL
-    )
-    system.communicate()
+        , stdout=DEVNULL, stderr=DEVNULL
+    ).communicate()
     return(inputFolder + ".zip")
 
-def packageCbz(file):
-    print ("original filename: " + file)
+def changeExt(file):
     newExtension = ".cbz"
     name, _ = splitext(file)
-    newName = name.replace("_optimized", "") + newExtension
-    print("new name: " + newName)
+    newName = name.replace("_compressed", "") + newExtension
+    print("creating file: " + newName)
     rename(file, newName)
 
-def compressCbzs(path):
+def deleteCompressed(path):
+    #find . -type d -name "*_optimized" -exec rm -rf {} \;
+
+    folders = Popen(
+        ['find', path, 
+        '-type', 'd', 
+        '-name', '*_compressed']
+        , stdout=PIPE, stderr=DEVNULL
+    ).communicate()[0]
+    folders = Popen(
+        ['sed', 's|\ |\\\ |g'], 
+        stdout=PIPE, stderr=DEVNULL, stdin=PIPE
+    ).communicate(folders)[0]
+
+    Popen(
+        ['xargs', 'rm', '-rf'], 
+        stdout=DEVNULL, stderr=DEVNULL, stdin=PIPE
+    ).communicate(folders) 
+    
+def deleteOriginals(path):
+    #find . -name "*.jp*g" -exec dirname {} \; | sed 's|\ |\\ |g' | uniq  | grep -v _optimized | xargs rm -rf
+
+    folders = Popen(
+        ['find', path, 
+        '-name', '*.jp*g', 
+        '-exec', 'dirname', '{}', ';']
+        , stdout=PIPE, stderr=DEVNULL
+    ).communicate()[0]
+    
+    folders = Popen(
+        ['sed', 's|\ |\\\ |g'], 
+        stdout=PIPE, stderr=DEVNULL, stdin=PIPE
+    ).communicate(folders)[0] 
+   
+    unique = Popen(
+        ['uniq'], 
+        stdout=PIPE, stderr=DEVNULL, stdin=PIPE
+    ).communicate(folders)[0]
+
+    filtered = Popen(
+        ['grep', '-v', '_compressed'], 
+        stdout=PIPE, stderr=DEVNULL, stdin=PIPE
+    ).communicate(unique)[0]
+    
+    Popen(
+        ['xargs', 'rm', '-rf'], stdout=DEVNULL, stdin=PIPE
+    ).communicate(filtered)
+    
+
+def packageCbzs(path, doCompress, doPack):
     for (dirpath, _, filenames) in walk(path):
         fileMatchers = ["JPG", "JPEG", "jpg", "jpeg"]
         files = [filename 
@@ -49,20 +101,59 @@ def compressCbzs(path):
                 if any(matcher in filename for matcher in fileMatchers)]
 
         if (len(files) > 0):   
-            optimizedFolder = optimize(dirpath)
-            zippedFolder = sevenzip(optimizedFolder)
-            packageCbz(zippedFolder)
+            zipPath = dirpath
+            if (doCompress):
+                zipPath = compress(dirpath)
             
-#TODO: prompt delete original and/or compressed folder
+            if (doPack):
+                zippedFolder = sevenzip(zipPath)
+                changeExt(zippedFolder)
+            
 #TODO: unpack original cbr/cbz
 #TODO: prompt to delete original cbr/cbz
-#TODO: make optimization optional
-        
-#TODO: make this more robust
-path = ""
-if len(sys.argv) > 1:
-    path = sys.argv[-1]
-else:
-    path = curdir
+#TODO: original size comparison original/packaged
 
-compressCbzs(path)
+def createArgParser():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--input", type=str, default=os.getcwd(), help="Input folder to pack, if empty, the working dir will be used")
+
+    ap.add_argument("-p" , "--pack", action='store_true', required=False,
+    help="Pack folders containing .jpg/.jpeg images into .cbz packages")
+
+    ap.add_argument("-c", "--compress", action='store_true', required=False,
+    help="If passed, lossy compression will be applied to the jpg images before packaging")
+
+    ap.add_argument("-sc", "--store-compressed", action='store_true', required=False,
+    help="If passed, the intermediate _compressed folders won't be deleted after completion")
+
+    ap.add_argument("-dc", "--delete-compressed", action='store_true', required=False,
+    help="If passed, the intermediate _compressed will be removed")
+
+    ap.add_argument("-do", "--delete-originals", action='store_true', required=False,
+    help="If passed, the original unpacked folders will be deleted after completion")
+
+    return ap
+
+argParser = createArgParser()
+args = argParser.parse_args()
+
+path = args.input
+doPack = args.pack
+doCompress = args.compress
+storeCompressed = args.store_compressed
+delCompressed = args.delete_compressed
+delOriginals = args.delete_originals
+
+print("args: " + str(args))
+
+if doPack | doCompress:
+    packageCbzs(path, doCompress, doPack)
+    if doCompress & (not storeCompressed):
+        deleteCompressed(path)
+
+if delCompressed:
+    print("Deleting compressed folders")
+    deleteCompressed(path)
+
+if delOriginals:
+    deleteOriginals(path)
